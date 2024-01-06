843444c5fec0c587e72a94d2244a8bba23c9d5a0
Kim-Tchoy <150024510+Kim-Tchoy@users.noreply.github.com>
2024-01-06 20:36:31 +0100

    Added Unit and Integration tests

---
+++ src/test/java/com/bezkoder/spring/jpa/h2/TestIntegration.java
@@ -0,0 +1,14 @@
+package com.bezkoder.spring.jpa.h2;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class TestIntegration {
+
+    @Test
+    public void testIntegrationExample() {
+
+        assertTrue(true, "Integration test should return 'true'.");
+    }
+
+}
---
+++ src/test/java/com/bezkoder/spring/jpa/h2/TestJUnit.java
@@ -0,0 +1,14 @@
+package com.bezkoder.spring.jpa.h2;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class TestJUnit {
+
+    @Test
+    public void testExample() {
+
+        assertEquals(2, 1 + 1, "The result should be 2");
+    }
+
+}
